0a9cc5983e84d49c1dd723dcf7d5c5f6159194b6
Donkey-Dude <oliver.nickerson@gmail.com>
2024-11-21 14:23:46 -0700

    grabber updates

--- grabber.js
+++ grabber.js
@@ -1,16 +1,21 @@
 function fractalize() {
     loadPixels();
-    done = 2;
       for (let w = 0; w < resox; w += pixsize) {
         for (let h = 0; h < resoy; h += pixsize) {
           for (let x = 0; x < pixsize; x++){
             for(let y = 0; y < pixsize; y++){
               grab = get(w+x, h+y);
-              rollingsum = rollingsum + grab[1];
+              rollingsum0 = rollingsum0 + grab[0];
+              rollingsum1 = rollingsum1 + grab[1];
+              rollingsum2 = rollingsum2 + grab[2];
             }
           }
-    valueArray.push(rollingsum / (pixsize * pixsize))
-    rollingsum = 0;
+    valueArray0.push(rollingsum0 / (pixsize * pixsize))
+    valueArray1.push(rollingsum1 / (pixsize * pixsize))
+    valueArray2.push(rollingsum2 / (pixsize * pixsize))
+    rollingsum0 = 0;
+    rollingsum1 = 0;
+    rollingsum2 = 0;
         }
       }
     }
